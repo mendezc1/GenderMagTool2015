258e42dc9921bc25a2dc49a0e63769f5cd432221
mendezc1 <mendezc@onid.oregonstate.edu>
2015-12-11 23:26:18 +0000

    Testing that hello world works Again

--- background.js
+++
@@ -1,8 +0,0 @@
-
-    function hello(){
-        chrome.tabs.query({currentWindow:true, active:true}, function(tabs){
-            console.log("in background.js");
-            chrome.tabs.insertHTML(tabs[0].id, {file: "helloWorld.html"});
-        });
-    }
-    
