1f7180bf582cd080924bfc187830c36af80fb53b
Taylor Cuilty <cuiltyt@onid.orst.edu>
2016-02-06 15:58:46 -0800

    Add ideal actions and enter responses to questions

--- popup.js
+++ popup.js
@@ -13,33 +13,44 @@ function takeScreenShot() {
 //chrome.browserAction.setPopup({popup: "new.html"});
 }
 
-function showQuestionForSubgoal() {
-	var subgoalQuestion = document.getElementById("questionForSubgoal");
-	subgoalQuestion.innerHTML = "Will " + personaName + " have formed this subgoal as a step to the overall goal?<br>";
-	var checkYes = document.createElement("input");
-	var yes = document.createElement("span");
-	var yesResponse = document.createElement("textArea");
-	yes.innerHTML = "Yes";
-	var no = document.createElement("span");
-	no.innerHTML = "No";
-	var checkNo = document.createElement("input");
-	var noResponse = document.createElement("textArea");
-	checkYes.type = "checkbox";
-	checkNo.type = "checkbox";
-	checkYes.value = "Yes";
-	checkNo.value = "No";
-	subgoalQuestion.appendChild(checkYes);
-	subgoalQuestion.appendChild(yes);
-	var linebreak1 = document.createElement("br");
-	subgoalQuestion.appendChild(linebreak1);
-	subgoalQuestion.appendChild(yesResponse);
-	var linebreak2 = document.createElement("br");
-	subgoalQuestion.appendChild(linebreak2);
-	subgoalQuestion.appendChild(checkNo);
-	subgoalQuestion.appendChild(no);
-	var linebreak3 = document.createElement("br");
-	subgoalQuestion.appendChild(linebreak3);
-	subgoalQuestion.appendChild(noResponse);
+function showQuestions(element, questions) {
+	chrome.extension.getBackgroundPage().console.log(questions.length);
+
+	for (var i = 0; i < questions.length; i++) {
+		chrome.extension.getBackgroundPage().console.log("Here!");
+		var question = document.createElement("span");
+		question.innerHTML = questions[i];
+		element.appendChild(question);
+		chrome.extension.getBackgroundPage().console.log("Here!");
+	
+		var checkYes = document.createElement("input");
+		var yes = document.createElement("span");
+		var yesResponse = document.createElement("textArea");
+		yes.innerHTML = "Yes";
+		var no = document.createElement("span");
+		no.innerHTML = "No";
+		var checkNo = document.createElement("input");
+		var noResponse = document.createElement("textArea");
+		checkYes.type = "checkbox";
+		checkNo.type = "checkbox";
+		checkYes.value = "Yes";
+		checkNo.value = "No";
+	
+		element.appendChild(checkYes);
+		element.appendChild(yes);
+		var linebreak1 = document.createElement("br");
+		element.appendChild(linebreak1);
+		element.appendChild(yesResponse);
+		var linebreak2 = document.createElement("br");
+		element.appendChild(linebreak2);
+		element.appendChild(checkNo);
+		element.appendChild(no);
+		var linebreak3 = document.createElement("br");
+		element.appendChild(linebreak3);
+		element.appendChild(noResponse);
+		var linebreak4 = document.createElement("br");
+		element.appendChild(linebreak4);
+	}
 }
 
 document.getElementById('clickme').addEventListener('click', takeScreenShot);
@@ -85,7 +96,8 @@ document.getElementById('btnSubmitSubtask').addEventListener('click', function(e
 		chrome.extension.getBackgroundPage().console.log("Subtask: ", subtaskName);
 		document.getElementById("subtaskName").innerHTML = "You selected " + subtaskName + " as your subtask";
 		
-		showQuestionForSubgoal();
+		var question = ["Will " + personaName + " have formed this subgoal as a step to the overall goal?<br>"];
+		showQuestions(document.getElementById("questionForSubgoal"), question);
 		
 		document.getElementById("subtaskInput").remove();
 		document.getElementById("btnSubmitSubtask").remove();
@@ -96,4 +108,24 @@ document.getElementById('btnSubmitSubtask').addEventListener('click', function(e
 
 // Add ideal action
 document.getElementById('addIdealAction').addEventListener('click', function(e) {
+	var question1 = "Will " + personaName + " even notice that the correct action is available?<br>";
+	var question2 = "Will " + personaName + " associate the correct action with the effect she is trying to achieve?<br>";
+	var question3 = "If the correct action is performed will " + personaName + " see that progress is being made toward a solution to her subgoal?<br>";
+	
+	var questions = [question1, question2, question3];
+	
+	var idealActions = document.getElementById("idealActions");
+	var idealAction = document.createElement("div");
+	idealActions.appendChild(idealAction);
+	
+	var actionName = document.createElement("span");
+	var actionNameInput = document.getElementById("actionNameInput");
+	actionName.innerHTML = actionNameInput.value + "<br>";
+	actionName.style = "font-weight:bold";
+	idealAction.appendChild(actionName);
+	
+	showQuestions(idealAction, questions);
+	
+	actionNameInput.value = "";
+	
 }, false);
